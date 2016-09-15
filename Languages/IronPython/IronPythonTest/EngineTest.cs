ffb878b2789dbf1ed082bcadecef352a991a79ac
Alex Earl <slide.o.mix@gmail.com>
2016-09-15 08:58:19 -0700

    Fix directory case

--- Languages/IronPython/IronPythonTest/EngineTest.cs
+++ Languages/IronPython/IronPythonTest/EngineTest.cs
@@ -70,10 +70,10 @@ namespace IronPythonTest {
             RuntimeDirectory = Path.GetDirectoryName(typeof(PythonContext).GetTypeInfo().Assembly.Location);
             RootDirectory = Environment.GetEnvironmentVariable("DLR_ROOT");
             if (RootDirectory != null) {
-                ScriptTestDirectory = Path.Combine(RootDirectory, "Languages", "IronPython", "Tests");
+                ScriptTestDirectory = Path.Combine(Path.Combine(Path.Combine(RootDirectory, "Languages"), "IronPython"), "Tests");
             } else {
                 RootDirectory = new System.IO.FileInfo(System.Reflection.Assembly.GetEntryAssembly().Location).Directory.FullName;
-                ScriptTestDirectory = Path.Combine(RootDirectory, "Src", "Tests");
+                ScriptTestDirectory = Path.Combine(Path.Combine(RootDirectory, "Src"), "Tests");
             }
             InputTestDirectory = Path.Combine(ScriptTestDirectory, "Inputs");
         }
@@ -312,7 +312,7 @@ namespace IronPythonTest {
 
             AreEqual(Python.GetSysModule(scriptEngine).GetVariable<string>("platform"), "cli");
             AreEqual(Python.GetBuiltinModule(scriptEngine).GetVariable<bool>("True"), true);
-            AreEqual(Python.ImportModule(scriptEngine, "nt").GetVariable<int>("F_OK"), 0);
+            AreEqual(Python.ImportModule(scriptEngine, "os").GetVariable<int>("F_OK"), 0);
             try {
                 Python.ImportModule(scriptEngine, "non_existant_module");
                 Assert(false);
@@ -332,7 +332,7 @@ namespace IronPythonTest {
 
             AreEqual(Python.GetSysModule(runtime).GetVariable<string>("platform"), "cli");
             AreEqual(Python.GetBuiltinModule(runtime).GetVariable<bool>("True"), true);
-            AreEqual(Python.ImportModule(runtime, "nt").GetVariable<int>("F_OK"), 0);
+            AreEqual(Python.ImportModule(runtime, "os").GetVariable<int>("F_OK"), 0);
             try {
                 Python.ImportModule(runtime, "non_existant_module");
                 Assert(false);
@@ -445,7 +445,7 @@ x = 42", scope);
 #if !SILVERLIGHT        
         public void ScenarioCodePlex20472() {
             try {
-                string fileName = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "encoded_files", "cp20472.py");
+                string fileName = Path.Combine(Path.Combine(System.IO.Directory.GetCurrentDirectory(), "encoded_files"), "cp20472.py");
                 _pe.CreateScriptSourceFromFile(fileName, System.Text.Encoding.GetEncoding(1251));
 
                 //Disabled. The line above should have thrown a syntax exception or an import error,
--- Makefile
+++ Makefile
@@ -17,13 +17,13 @@ testrunner-release:
 test-ipy: ironpython testrunner
 	CONFIGURATION=Debug DLR_ROOT=`pwd` DLR_BIN=`pwd`bin/Debug \
 		      mono Test/TestRunner/TestRunner/bin/Debug/TestRunner.exe \
-		      Test/IronPython.tests.linux /verbose /binpath:bin/Debug /all
+		      Test/IronPython.tests /verbose /binpath:bin/Debug /all
 
 
 test-ipy-release: ironpython-release testrunner-release
 	CONFIGURATION=Release DLR_ROOT=`pwd` DLR_BIN=`pwd`bin/Release \
 		      mono Test/TestRunner/TestRunner/bin/Release/TestRunner.exe \
-		      Test/IronPython.tests.linux /verbose /binpath:bin/Release /all
+		      Test/IronPython.tests /verbose /binpath:bin/Release /all
 
 clean:
 	xbuild Build.proj /t:Clean /p:Mono=true
